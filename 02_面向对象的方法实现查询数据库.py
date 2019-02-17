890d22106241518b66e3d5a5834e71ec80a3ba98
Ratatouille <307338947@qq.com>
2019-02-17 11:12:34 +0800

    加入登录和退出

--- 02_面向对象的方法实现查询数据库.py
+++ 02_面向对象的方法实现查询数据库.py
@@ -6,26 +6,45 @@ class HandleDatabase(object):
         self.conn = pymysql.connect(host="localhost", port=3306, user="root", password="123456", database="jing_dong",
                                     charset="utf8")
         self.cursor1 = self.conn.cursor()
+        self.current_user = str()
 
     def start_page(self):
-        user_name = input("请输入用户名：")
-        user_password = input("请输入密码：")
-        count = self.cursor1.execute("""select * from jing_dong.customers where name = "%s" """ % user_name)
-        if count > 0:
-            print(self.cursor1.fetchall())
-            count_pass = self.cursor1.execute(
-                """select * from jing_dong.customers where name = "%s" and password = "%s" """ % (
-                user_name, user_password))
-            if count_pass > 0:
+        while True:
+            user_name = input("请输入用户名：")
+            user_password = input("请输入密码：")
+            count = self.cursor1.execute("""select * from jing_dong.customers where name = "%s" """ % user_name)
+            if count > 0:
                 print(self.cursor1.fetchall())
-                print("登录成功！\r\n")
+                count_pass = self.cursor1.execute(
+                    """select * from jing_dong.customers where name = "%s" and password = "%s" """ % (
+                        user_name, user_password))
+                if count_pass > 0:
+                    print(self.cursor1.fetchall())
+                    print("用户%s登录成功！\r\n" % user_name)
+                    self.current_user = user_name
+                    break
 
-        else:
-            print("用户不存在，请注册：")
-            self.user_regist()
+            else:
+                print("用户不存在，请注册：")
+                self.user_regist()
 
     def user_regist(self):
-        pass
+        while True:
+            print("这里是注册函数")
+            user_name = input("请输入用户名:")
+            count = self.cursor1.execute("""select * from jing_dong.customers where name = "%s" """ % user_name)
+            if count > 0:
+                print("用户名已存在，请重新输入：")
+            else:
+                break
+        user_password = input("请输入密码：")
+        user_address = input("请输入地址：")
+        user_tel = input("请输入电话")
+
+        self.cursor1.execute("""insert into customers values(
+        0,"%s","%s","%s","%s") """ % (
+            user_name, user_address, user_tel, user_password))
+        self.conn.commit()
 
     @staticmethod
     def show_operations():
@@ -34,24 +53,27 @@ class HandleDatabase(object):
         print("2. 查询所有类别：\n\r")
         print("3. 查询所有品牌：\n\r")
         print("4. 添加一个商品分类 \n\r")
+        print("5. 退出登录 \n\r")
 
-    def make_sql(self):
-        option = input("请输入操作：\n\r")
-        if option == '1':
-            sql = "select * from goods;"
-            self.execute_sql(sql)
-        elif option == '2':
-            sql = "select * from goods_cates;"
-            self.execute_sql(sql)
-        elif option == '3':
-            sql = "select * from goods_brands;"
-            self.execute_sql(sql)
-        elif option == '4':
-            name_cate = input("请输入名字")
-            sql = """insert into goods_cates values (0, "%s");""" % name_cate
-            self.execute_write(sql)
-        else:
-            pass
+    # def make_sql(self):
+    #     option = input("请输入操作：\n\r")
+    #     if option == '1':
+    #         sql = "select * from goods;"
+    #         self.execute_sql(sql)
+    #     elif option == '2':
+    #         sql = "select * from goods_cates;"
+    #         self.execute_sql(sql)
+    #     elif option == '3':
+    #         sql = "select * from goods_brands;"
+    #         self.execute_sql(sql)
+    #     elif option == '4':
+    #         name_cate = input("请输入名字")
+    #         sql = """insert into goods_cates values (0, "%s");""" % name_cate
+    #         self.execute_write(sql)
+    #     elif option == '5':
+    #         self.log_off()
+    #     else:
+    #         pass
 
     def execute_sql(self, sql):
         count = self.cursor1.execute(sql)
@@ -68,13 +90,36 @@ class HandleDatabase(object):
         self.cursor1.close()
         self.conn.close()
 
+    def log_off(self):
+        self.current_user = "NULL"
+
     def run(self):
         while True:
-            self.show_operations()
-            self.make_sql()
+            self.start_page()
+            while True:
+                self.show_operations()
 
-            # self.execute_sql()
-            # 1self.close_connections()
+                option = input("请输入操作：\n\r")
+                if option == '1':
+                    sql = "select * from goods;"
+                    self.execute_sql(sql)
+                elif option == '2':
+                    sql = "select * from goods_cates;"
+                    self.execute_sql(sql)
+                elif option == '3':
+                    sql = "select * from goods_brands;"
+                    self.execute_sql(sql)
+                elif option == '4':
+                    name_cate = input("请输入名字")
+                    sql = """insert into goods_cates values (0, "%s");""" % name_cate
+                    self.execute_write(sql)
+                elif option == '5':
+                    self.log_off()
+                    break
+                else:
+                    pass
+                # self.execute_sql()
+                # 1self.close_connections()
 
 
 def main():
